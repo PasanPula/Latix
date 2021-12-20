a7f45f2f4f21935ffd15de004df354cb57c3a48b
PasanPula <ppulasithi@gmail.com>
2021-12-20 16:25:21 +0530

    fix multiplayer result render

--- src/MultiPlayer/result/Multiplayer.result.js
+++ src/MultiPlayer/result/Multiplayer.result.js
@@ -1,30 +1,23 @@
-import {useState,useEffect} from 'react'
-
+import {useState} from 'react'
 export default function MultiplayerResult({userResult}) {
 
-    const Result = userResult.map((data,index) => {
+    const[result] = useState(userResult);
+
+    const Result = result.map((data,index) => {
         return (
             <li key={index}>
-             <label style={{color:'white'}} >{index+1}: </label>
+             <label style={{color:'white'}} >{index+1}: </label> 
                <label style={{color:'White'}} > {data.ClientName} </label>
                <label style={{color:'White'}} > {data.Result.Time} </label>
                <label style={{color:'White'}} > {data.Result.Correct} </label>
                <label style={{color:'White'}} > {data.Result.InCorrect} </label>
             </li>
          );
-    });
+    }); 
 
-    // useEffect(() => {
-    //     console.log(userResult);
-    // },)
-
-
-
-//8qHE7
     return (
         <div>
-            <Result/>
-            {/* result */}
+            { Result }
         </div>
     )
 }
