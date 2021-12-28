f9f3fecbcaa08239e25413cde549f78bd0f517a8
PasanPula <ppulasithi@gmail.com>
2021-12-28 15:27:23 +0530

    multiplayer play again lobby push

--- src/MultiPlayer/result/Multiplayer.result.js
+++ src/MultiPlayer/result/Multiplayer.result.js
@@ -1,41 +1,58 @@
-import {useState} from 'react';
+import { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+import { useSocket } from "../../Services/SocketProvider";
 
+export default function MultiplayerResult({ isCreator, userResult }) {
+  const [result] = useState(userResult);
+  const history = useHistory();
+  const socket = useSocket();
 
-export default function MultiplayerResult({isCreator,userResult}) {
-
-    const[result] = useState(userResult);
-    const history = useHistory();
-
-    const Result = result.map((data,index) => {
-        return (
-            <li key={index}>
-             <label style={{color:'white'}} >{index+1}: </label> 
-               <label style={{color:'White'}} > {data.ClientName} </label>
-               <label style={{color:'White'}} > {data.Result.Time} </label>
-               <label style={{color:'White'}} > {data.Result.Correct} </label>
-               <label style={{color:'White'}} > {data.Result.InCorrect} </label>
-            </li>
-         );
-    }); 
-
-    const handelPlayAgain = () =>
+  useEffect(() => {
+    
+    if(!isCreator)
     {
-        history.push('/Multiplay/create');
+        socket.on("UpdateGame", (res) => {
+            console.log("result", res);
+      
+            if(res.IsUpdate)
+            {
+              history.push("/Multiplay/lobby");
+            }
+          });
     }
 
+    return () => {
+        socket.off("UpdateGame");
+    }
+  });
+
+  const Result = result.map((data, index) => {
     return (
-        <div>
-            { Result }
-            <div>
-                {isCreator?
-                <Button onClick={handelPlayAgain} >
-                    Play Again
-                </Button>
-                :
-                <span></span>}
-            </div>
-        </div>
-    )
+      <li key={index}>
+        <label style={{ color: "white" }}>{index + 1}: </label>
+        <label style={{ color: "White" }}> {data.ClientName} </label>
+        <label style={{ color: "White" }}> {data.Result.Time} </label>
+        <label style={{ color: "White" }}> {data.Result.Correct} </label>
+        <label style={{ color: "White" }}> {data.Result.InCorrect} </label>
+      </li>
+    );
+  });
+
+  const handelPlayAgain = () => {
+    history.push("/Multiplay/create");
+  };
+
+  return (
+    <div>
+      {Result}
+      <div>
+        {isCreator ? (
+          <Button onClick={handelPlayAgain}>Play Again</Button>
+        ) : (
+          <span></span>
+        )}
+      </div>
+    </div>
+  );
 }
