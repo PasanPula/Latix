e8d349ca7fc90f8251870195037d6afaa7d1c877
PasanPula <ppulasithi@gmail.com>
2022-01-09 16:54:23 +0530

    Home layout add

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,27 +1,76 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
-import './Home.css'
+import "./Home.css";
 
-const Home = ()=>{
-    return(
-        <div >
-            <div className="mb-2 center">
-                <Link to="/Singleplayer">
-                    <Button variant="primary" size="lg" className="button">
-                        Single Play
-                    </Button>
-                </Link>
-                <br/>
-                <Link to="/Multiplay">
-                    <Button variant="primary" size="lg" className="button">
-                        Multi Play
-                    </Button>
-                </Link>
-            </div>
-            {/* <Link to="/Single_play">Single Play</Link> */}
+const Home = () => {
+  return (
+    // <div >
+    //     <div className="mb-2 center">
+    //         <Link to="/Singleplayer">
+    //             <Button variant="primary" size="lg" className="button">
+    //                 Single Play
+    //             </Button>
+    //         </Link>
+    //         <br/>
+    //         <Link to="/Multiplay">
+    //             <Button variant="primary" size="lg" className="button">
+    //                 Multi Play
+    //             </Button>
+    //         </Link>
+    //     </div>
+    // </div>
+
+    <div class="container-fluid vh-100 ">
+      <div class="row  h-10">
+        <div style={{ backgroundColor: "red" }} class="col-md-12">
+          logo
         </div>
-    );
-}
+      </div>
+      <div class="row h-80 align-items-center">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 ">
+          <div class="row">
+            <div class="col-md-12 d-flex justify-content-center">
+              <Link to="/Singleplayer">
+                <button
+                  variant="primary"
+                  size="lg"
+                  className="btnGame learn-more home-button"
+                >
+                  Single Play
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div class="row pt-4 ">
+            <div class="col-md-12 d-flex justify-content-center">
+              <Link to="/Multiplay">
+                <button
+                  variant="primary"
+                  size="lg"
+                  className="btnGame learn-more home-button"
+                >
+                  Multi Play
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3"></div>
+      </div>
+      <div class="row bottom-bar  h-10">
+        <div class="col-md-12 ">
+          <div class="row ">
+            <div class="col-md-3 ">Terms & conditions</div>
+            <div class="col-md-3 ">Privacy policy</div>
+            <div class="col-md-3 ">Contact us</div>
+            <div class="col-md-3 ">Contact us</div>
+          </div>
+          Allrigh
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
