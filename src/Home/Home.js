409c5c7682bff61b2988fc996f9e86abc1b6939b
PasanPula <ppulasithi@gmail.com>
2022-01-12 14:43:41 +0530

    add mode page layout

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,45 +5,45 @@ import { IoIosPeople, IoIosPerson } from "react-icons/io";
 
 const Home = () => {
   return (
-    <div class="container-fluid vh-100 ">
-      <div class="row  h-10">
-        <div style={{ backgroundColor: "red" }} class="col-md-12">
+    <div className="container-fluid vh-100 ">
+      <div className="row  h-10">
+        <div style={{ backgroundColor: "red" }} className="col-md-12">
           logo
         </div>
       </div>
-      <div class="row h-20 align-items-center ">
-        <div class="col-md-12  justify-content-center">
-            <div class="penguin">
-              <div class="penguin-bottom">
-                <div class="right-hand"></div>
-                <div class="left-hand"></div>
-                <div class="right-feet"></div>
-                <div class="left-feet"></div>
+      <div className="row h-20 align-items-center ">
+        <div className="col-md-12  justify-content-center">
+            <div className="penguin">
+              <div className="penguin-bottom">
+                <div className="right-hand"></div>
+                <div className="left-hand"></div>
+                <div className="right-feet"></div>
+                <div className="left-feet"></div>
               </div>
-              <div class="penguin-top">
-                <div class="right-cheek"></div>
-                <div class="left-cheek"></div>
-                <div class="belly"></div>
-                <div class="right-eye">
-                  <div class="sparkle"></div>
+              <div className="penguin-top">
+                <div className="right-cheek"></div>
+                <div className="left-cheek"></div>
+                <div className="belly"></div>
+                <div className="right-eye">
+                  <div className="sparkle"></div>
                 </div>
-                <div class="left-eye">
-                  <div class="sparkle"></div>
+                <div className="left-eye">
+                  <div className="sparkle"></div>
                 </div>
-                <div class="blush-right"></div>
-                <div class="blush-left"></div>
-                <div class="beak-top"></div>
-                <div class="beak-bottom"></div>
+                <div className="blush-right"></div>
+                <div className="blush-left"></div>
+                <div className="beak-top"></div>
+                <div className="beak-bottom"></div>
             </div>
           </div>
           <div className="Welcome-text">Welcome to Latix </div>
         </div>
       </div>
-      <div class="row h-60 align-items-center">
-        <div class="col-md-3"></div>
-        <div class="col-md-6 ">
-          <div class="row">
-            <div class="col-md-12 d-flex justify-content-center">
+      <div className="row h-60 align-items-center">
+        <div className="col-md-3"></div>
+        <div className="col-md-6 ">
+          <div className="row">
+            <div className="col-md-12 d-flex justify-content-center">
               <Link to="/Singleplayer">
                 <button
                   variant="primary"
@@ -56,8 +56,8 @@ const Home = () => {
               </Link>
             </div>
           </div>
-          <div class="row pt-4 ">
-            <div class="col-md-12 d-flex justify-content-center">
+          <div className="row pt-4 ">
+            <div className="col-md-12 d-flex justify-content-center">
               <Link to="/Multiplay">
                 <button
                   variant="primary"
@@ -71,23 +71,23 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div class="col-md-3"></div>
+        <div className="col-md-3"></div>
       </div>
-      <div class="row bottom-bar  h-10">
-        <div class="col-md-12 ">
-          <div class="row justify-content-center mt-3">
-            <div class="col-md-4 d-flex justify-content-center bottom-text">
+      <div className="row bottom-bar  h-10">
+        <div className="col-md-12 ">
+          <div className="row justify-content-center mt-2">
+            <div className="col-md-4 d-flex justify-content-center bottom-text">
               Terms & conditions
             </div>
-            <div class="col-md-4 d-flex justify-content-center bottom-text">
+            <div className="col-md-4 d-flex justify-content-center bottom-text">
               Privacy policy
             </div>
-            <div class="col-md-4 d-flex justify-content-center bottom-text">
+            <div className="col-md-4 d-flex justify-content-center bottom-text">
               Contact us
             </div>
           </div>
-          <div class="row justify-content-center mt-3">
-            <div class="col-md-12 d-flex justify-content-center">
+          <div className="row justify-content-center mt-2">
+            <div className="col-md-12 d-flex justify-content-center">
               Made With ❤️ by USJ
             </div>
           </div>
--- src/MultiPlayer/MultiPlayer.mode.js
+++ src/MultiPlayer/MultiPlayer.mode.js
@@ -1,33 +1,68 @@
-import { Link } from 'react-router-dom';
-import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./mode/Multiplayer.mode.css";
+import { FaEdit } from "react-icons/fa";
+import { AiFillPlusSquare } from "react-icons/ai";
 
+export default function MultiPlayerMode({ HandleShowCheckBox }) {
+  const createButtonClick = () => {
+    HandleShowCheckBox(true);
+  };
 
-export default function MultiPlayerMode({HandleShowCheckBox}) {
+  return (
+    // <div>
+    //      <div >
+    //     <div className="mb-2 center">
+    //         <Link to="/Multiplay/Login">
+    //             <button variant="primary" size="lg" className="button">
+    //                 Join
+    //             </button>
+    //         </Link>
+    //         <br/>
+    //         <Link to="/Multiplay/login">
+    //             <button onClick={createButtonClick} variant="primary" size="lg" className="button">
+    //                 Create
+    //             </button>
+    //         </Link>
+    //     </div>
+    // </div>
+    // </div>
 
-   
-
-     const  createButtonClick = () =>
-    {
-        HandleShowCheckBox(true);
-    }
-
-    return (
-        <div>
-             <div >
-            <div className="mb-2 center">
-                <Link to="/Multiplay/Login">
-                    <Button variant="primary" size="lg" className="button">
-                        Join
-                    </Button>
-                </Link>
-                <br/>
-                <Link to="/Multiplay/login">
-                    <Button onClick={createButtonClick} variant="primary" size="lg" className="button">
-                        Create
-                    </Button>
-                </Link>
-            </div>
+    <div className="container-fluid vh-100">
+      <div className="row  h-10">
+        <div className="col-md-12" style={{ backgroundColor: "red" }}>
+          Logo
         </div>
+      </div>
+      <div className="row align-items-end h-20">
+        <div className="col-md-12">
+          <label className="mode-title"> What Do You Want To Do? </label>
         </div>
-    )
+      </div>
+      <div className="row h-60 align-items-center">
+        <div className="col-md-1"></div>
+        <div className="col-md-5 d-flex justify-content-end">
+          <Link to="/Multiplay/Login">
+            <button className="mode-button">
+              <AiFillPlusSquare className="mode-icon"/>
+              <br></br>
+              Join Game
+            </button>
+          </Link>
+        </div>
+        <div className="col-md-5 d-flex justify-content-start">
+          <Link to="/Multiplay/login">
+            <button onClick={createButtonClick} className="mode-button">
+              <FaEdit className="mode-icon"/>
+              <br></br>
+              Create Game
+            </button>
+          </Link>
+        </div>
+        <div className="col-md-1"></div>
+      </div>
+      <div className="row h-10">
+        <div className="col-md-12"></div>
+      </div>
+    </div>
+  );
 }
