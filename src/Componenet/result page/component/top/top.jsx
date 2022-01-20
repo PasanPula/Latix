46a1270e5da9cd4e1ab13cbadf6dedf67692a591
PasanPula <ppulasithi@gmail.com>
2022-01-21 01:33:30 +0530

    add missing css class

--- src/Componenet/result page/component/top/top.jsx
+++ src/Componenet/result page/component/top/top.jsx
@@ -12,10 +12,12 @@ export function Top({ place, No, Image, Name, time, correct, incorrect }) {
       >
         <img  className="pic" src={Image} alt="avatar" />
       </div>
+      <div className="top-body">
       <div className="name">{Name}</div>
       <div className="score">Time : {time}</div>
       <div className="score">Correct : {correct}</div>
       <div className="score">Incorrect : {incorrect}</div>
+      </div>
     </div>
   );
 }
