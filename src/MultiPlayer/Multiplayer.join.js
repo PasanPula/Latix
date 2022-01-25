1c79d559281d7c4c39e32694cf271756f7102cf9
PasanPula <ppulasithi@gmail.com>
2022-01-26 05:04:56 +0530

    fix multiplayer join code pages issues

--- src/MultiPlayer/Multiplayer.join.js
+++ src/MultiPlayer/Multiplayer.join.js
@@ -5,8 +5,6 @@ import "./join/Multiplayer.join.css";
 import { IconButton } from "../Componenet/Button/ButtonIcon/Button";
 import { faHome, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
-
-
 export default function MultiplayerJoin({ name, setGameId, avatar }) {
   const [gameCode, setGameCode] = useState("");
   const socket = useSocket();
@@ -36,6 +34,14 @@ export default function MultiplayerJoin({ name, setGameId, avatar }) {
     );
   };
 
+  const onGoBackHome = () => {
+    history.push("/");
+  };
+
+  const onGoBack = () => {
+    history.push("/Multiplay/Login");
+  };
+
   return (
     //     <div>
 
@@ -56,43 +62,28 @@ export default function MultiplayerJoin({ name, setGameId, avatar }) {
     // </div>
     <div className="container-fluid vh-100">
       <div className="row align-items-center text-center   h-10">
-          <div
-            className="col-md-4 "
-          >
-          </div>
-          <div
-            className="col-md-4 position-front"
-          >
-             <img
-                className="logo-style"
-                src={`/Assets/logo.svg`}
-                alt="Latix"
-              />
-          </div>
-          <div
-            className="col-md-4 position-front"
-          >
-          </div>
+        <div className="col-md-4 "></div>
+        <div className="col-md-4 position-front">
+          <img className="logo-style" src={`/Assets/logo.svg`} alt="Latix" />
         </div>
-        <div className="row justify-content-start">
+        <div className="col-md-4 position-front"></div>
+      </div>
+      <div className="row justify-content-start">
         <div className="col-md-2 text-center ">
           <IconButton
             icon={faArrowLeft}
             height={"80px"}
             width={"100px"}
             fontSize={"40px"}
+            onClick={onGoBack}
           />
         </div>
-        <div className="col-md-8 p-3 justify-content-center ">
-        
-        </div>
-        <div className="col-md-2 ">
-          
-        </div>
+        <div className="col-md-8 p-3 justify-content-center "></div>
+        <div className="col-md-2 "></div>
       </div>
       <div className="row align-items-end h-40">
         <div className="col-md-12">
-          <div className="row pt-5 justify-content-center">
+          <div className="row pt-3 justify-content-center">
             <div className="col-md-12 d-flex justify-content-center">
               <img
                 className="join-avatar position-front"
@@ -103,17 +94,28 @@ export default function MultiplayerJoin({ name, setGameId, avatar }) {
           </div>
           <div className="row pt-2 justify-content-center">
             <div className="col-md-12">
-              <label className="join-title-1   position-front"> Hi {name} {avatar} </label>
+              <label className="join-title-1   position-front">
+                {" "}
+                Hi {name} {avatar}{" "}
+              </label>
             </div>
           </div>
         </div>
       </div>
       <div className="row align-items-center">
         <div className="col-md-2"></div>
-        <div className="col-md-8  d-flex justify-content-center">
-          <div>
-          <label className="join-textbox-text pt-2  position-front"> Enter Your Game code Here </label>
+        <div className="col-md-8  ">
           <div className="row pt-2 justify-content-center">
+            <div className="col-md-12  d-flex justify-content-center">
+              <label className="join-textbox-text pt-2  position-front">
+               Enter Your Game code Here
+              </label>
+              <div></div>
+            </div>
+          </div>
+          <div>
+          <div className="row pt-2 justify-content-center">
+          <div className="col-md-12  d-flex justify-content-center">
             <input
               className="input-field  position-front"
               placeholder="Code"
@@ -123,34 +125,33 @@ export default function MultiplayerJoin({ name, setGameId, avatar }) {
               onChange={handleGameCode}
               required
             />
-          </div>
-          <div className="row pt-3 justify-content-center">
-            {/* <button onClick={onSubmit} className="btnGame learn-more join-btn  position-front">
-              Join
-            </button> */}
-            <button
-                    onClick={onSubmit}
-                    type="submit"
-                    className="custom-btn btn-2 "
-                    style={{ width: "150px", height: "55px", fontSize: "35px" }}
-                  >
-                    <div className="btn-con">Login</div>
-                  </button>
+            </div>
           </div>
           </div>
-
         </div>
         <div className="col-md-2"></div>
       </div>
-      <div className="row  justify-content-start">
-        <div className="col-md-2 p-4 text-center">
+      <div className="row  align-items-center">
+        <div className="col-md-2 p-2 text-center">
           <IconButton
             icon={faHome}
             height={"80px"}
             width={"100px"}
             fontSize={"40px"}
+            onClick={onGoBackHome}
           />
         </div>
+        <div className="col-md-8 text-center">
+          <button
+            onClick={onSubmit}
+            type="submit"
+            className="custom-btn btn-2 "
+            style={{ width: "150px", height: "55px", fontSize: "35px" }}
+          >
+            <div className="btn-con">Login</div>
+          </button>
+        </div>
+        <div className="col-md-2  text-center"></div>
       </div>
     </div>
   );
